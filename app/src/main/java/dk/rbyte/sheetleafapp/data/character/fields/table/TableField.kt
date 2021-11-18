47b05479e1617388b59f8f297292de7aa755f609
lizator <frederikkoefoeds@gmail.com>
2021-11-18 08:47:51 +0100

    Added FieldTypes

--- app/src/main/java/dk/rbyte/sheetleafapp/data/character/fields/table/TableField.kt
+++ app/src/main/java/dk/rbyte/sheetleafapp/data/character/fields/table/TableField.kt
@@ -1,11 +1,13 @@
 package dk.rbyte.sheetleafapp.data.character.fields.table
 
+import dk.rbyte.sheetleaf.data.character.fields.FieldTypes
 import dk.rbyte.sheetleafapp.data.character.fields.DataField
 
 data class TableField(
     override var id: String,
     override var characterID: Int,
     override var title: String,
-    override var value: Any
+    override var value: Any,
+    override var type: FieldTypes = FieldTypes.TABLE_FIELD
 ) : DataField {
 }
